769dfc8d07fd029928dd1044390c61bd0ee385c7
Xiong Changnian <xiong@cpan.org>
2010-09-15 18:58:41 -0700

    Some preparatory work.
    * Eliminate all TR stuff.
    * Fix metadata.
    * Write main Bundle::Cheat,
    	which is also the model for other BC::* modules.
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Bundle/Cheat.pm
+++ lib/Bundle/Cheat.pm
@@ -7,13 +7,20 @@ use warnings;
 use version 0.77; our $VERSION = qv('0.0.1');
 
 ## END MODULE
-1;
+return 1;   # Abort execution; fool syntax highlighting for following code.
+die 'Failed to abort. This line should never execute; please contact author.';
+
+#============================================================================#
+#=========# ALL CHEAT MODULES
+
+use Test::Cheat;
+
 #============================================================================#
 __END__
 
 =head1 NAME
 
-Bundle::Cheat - Chewy target for your pitbull testing module
+Bundle::Cheat - Copy-and-paste usage lines for lazy coders
 
 =head1 VERSION
 
@@ -21,23 +28,55 @@ This document describes Bundle::Cheat version 0.0.1
 
 =head1 SYNOPSIS
     
-    use Bundle::Cheat;
+    $ cat Bundle/Cheat.pm
 
 =head1 DESCRIPTION
 
+I<If a thing is worth having, it's worth cheating for.> 
+--W. C. Fields
+
+This is a collection of "cheat sheets": highly compressed, abbreviated 
+documentation for various modules. Each module within the bundle covers a 
+top-level namespace or a set of otherwise closely-related modules. 
+
+For each module, a paragraph is given, generally: 
+
+    Some::Module            # Short description
+        qw( various exportable symbols if any );
+        routine( $placeholder, @arguments );
+        $context    = function( @arguments);
+        $object->method();
+
+You should be able to copy and paste this into your own code, 
+delete what you don't need, and be on your way. 
 
 =head1 FUNCTIONS/METHODS
 
+None. This module does nothing at all; you shouldn't even try to C<use()> it,
+although it's harmless. Same goes for all *::Cheat modules. 
 
 =head1 INTERFACE 
 
+Open the module (the *.pm file itself) in your editor. 
+Copy out whatever you like. 
 
 =head1 DIAGNOSTICS
 
+=over
+
+=item Failed to abort. This line should never execute; please contact author.
+
+This module contains executable code so that your editor will use
+its syntax highlighting feature, if any. But it would be madness to execute it
+all. Of the various strategies for suppressing execution of the cheat sheet, 
+the one chosen is to C<return 1;> unconditionally. I can't imagine how this 
+can fail; but if it does, I hope we will C<die()> before going further. 
+
+=back
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-Acme::Teddy requires no configuration files or environment variables.
+Bundle::Cheat requires no configuration files or environment variables.
 
 =head1 DEPENDENCIES
 
@@ -49,6 +88,12 @@ None.
 
 =head1 BUGS AND LIMITATIONS
 
+No cheat sheet will teach you anything. It's only a reminder. You B<must> 
+consult each module's own full documentation I<at least> before using it. 
+I hope. 
+
+This module does not contain magic to squirt code into your module. 
+Copy and paste. 
 
 =head1 THANKS
 
@@ -56,6 +101,7 @@ None.
 
 =item *
 
+To about 8500 authors who have uploaded about 85,000 modules to the CPAN. 
 
 =back
 
