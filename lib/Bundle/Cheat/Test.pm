2929bedab09e610e168ed6a96067f750f21cfc35
Xiong Changnian <xiong@cpan.org>
2010-09-18 03:03:25 -0700

    Add Test::Trap.
    * Include demo test-trap-01.t
    * Not sure how far to go with that.
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Bundle/Cheat/Test.pm
+++ lib/Bundle/Cheat/Test.pm
@@ -16,6 +16,7 @@ die 'Failed to abort. This line should never execute; please contact author.';
 use Test::Simple tests => 6;    # Basic utilities for writing tests
     ok( $bool, $name );             # ok if $bool is true
     ok( $foo eq $bar, $name );      # ok if $foo eq $bar
+## Test::Simple
 
 use Test::More tests => 6;      # Standard framework for writing test scripts
     ok  ( $bool, $name );               # ok if $bool is true
@@ -42,6 +43,7 @@ use Test::More;                     # declare number of tests later
     my @dump = explain( @refs );            # uses Data::Dumper
     my @dump = explain( \@array, \%hash );  #   to dump list of references
     BAIL_OUT( $reason );        # abort this and all following test scripts
+## Test::More
 
 use Test::Deep;                 # Extremely flexible deep comparison
     cmp_deeply( $got, $want, $name );   # ok if $got eq $want deeply
@@ -70,7 +72,90 @@ use Test::Deep;                 # Extremely flexible deep comparison
         ),
     };
     cmp_deeply( $got, $cmp,  $name ); # ok if $got special $cmp deeply
+## Test::Deep
 
+use Test::Trap;                 # Trap exit codes, exceptions, output, etc.
+# $trap object is exported into your namespace and contains everything.
+# Methods can be combined in a large variety of ways; see Test::Trap POD.
+use Test::Trap  qw( :raw :die :exit 
+                        :flow 
+                    :stdout             :stderr 
+                    :stdout(perlio)     :stderr(perlio)
+                    :stdout(tempfile)   :stderr(tempfile)
+                    :stdout(method)     :stderr(method)
+                    :stdout( m1, m2, m3 )
+                    :warn
+                        :default
+                    :on_fail(method)
+                    :void       :scalar     :list
+                    :output(systemsafe)     :output(method)
+);
+use Test::Trap(     # order of layers in the use-array is significant
+        ':raw',                 # traps normal return and stops trapping
+        ':die',                 # traps fatal exceptions
+        ':exit',                # traps attempts to exit() perl
+    ':flow',                # shortcut for :raw:die:exit
+        ':stdout',              # trap STDOUT
+        ':stderr',              # trap STDERR
+            ':stdout(perlio)',      # trap using PerlIO::scalar
+            ':stdout(tempfile)',    # trap using File::Temp to a tempfile
+            ':stdout(method)',      # trap using some user method
+            ':stdout(m1,m2,m3)',    # provide a list of fallback methods
+        ':warn',                # trap warnings and tee them to STDERR
+    ':default',             # shortcut for :raw:die:exit:stdout:stderr:warn
+        ':on_fail(method)',     # user method to callback on fatals
+    ':void',                # return in scalar context
+    ':scalar',              # return in list   context
+    ':list',                # return in void   context
+        ':output(systemsafe)',  # traps children including system calls
+        ':output(method)',      # fallback processing with user method
+);
+    # If you want the normal return value from code under test, 
+    #       use Test::Trap qw( :scalar );
+    #       use Test::Trap qw( :list );
+    #   or  provide a context yourself with:
+    my $rv  = trap{  };     # return in scalar context
+    my @rvs = trap{  };     # return in list   context
+    trap{                   # return in void   context
+        # Your code under test here
+    };
+    $trap->diag_all;                    # Dumps the $trap object, TAP safe
+    # Accessor methods  # ACC
+    my $got = $trap->leaveby;           # 'return', 'die', or 'exit'. 
+    my $got = $trap->die;               # exception thrown if any 
+    my $got = $trap->exit;              # exit code caught if attempted
+    my $got = $trap->stdout;            # STDOUT in one string
+    my $got = $trap->stderr;            # STDERR in one string
+    my $got = $trap->return;            # arrayref of normal return values
+    my $got = $trap->return($index);    # pass $index as method argument
+    my $got = $trap->return(@indices);  # it slices, it dices
+    my $got = $trap->warn  ($index);    # warnings as an array
+    # Test methods  (for any ACC)
+    #   e.g.:       $trap->return_ok();   $trap->stdout_like();
+    #   $ix or @ixs required if ACC is array, otherwise omit
+    use Test::More tests => 9;                      # ~~ Test::More::*
+    $trap->ACC_ok       ( $ix, $name );             #              ok()
+    $trap->ACC_nok      ( $ix, $name );             #       !      ok()
+    $trap->ACC_is       ( $ix, $want,  $name );     #              is()
+    $trap->ACC_isnt     ( $ix, $want,  $name );     #       !      is()
+    $trap->ACC_like     ( $ix, qr/./,  $name );     #            like()
+    $trap->ACC_unlike   ( $ix, qr/./,  $name );     #       !    like()
+    $trap->ACC_isa_ok   ( $ix, $class, $name );     #             isa()
+    $trap->ACC_is_deeply( $ix, $want,  $name );     #       is_deeply()
+    # Examples of above: 
+    $trap->return_ok( 0, 'got something' ); # even if :scalar, return => []
+    $trap->return_is( 1, 9,  'returns an array and the second element is 9' );
+    $trap->stdout_like( qr/hell|damn/, 'tried to print a word to screen'); 
+    $trap->die_unlike ( qr/hell|damn/, 'died like a lady');  # fail if !die
+    $trap->return_isa_ok ( 0, 'Acme::Teddy' 'returned object');
+    # Convenience methods with better diagnostics than their equivalents...
+    $trap->did_die;         # $trap->leaveby_is('die');
+    $trap->did_exit;        # $trap->leaveby_is('exit');
+    $trap->did_return;      # $trap->leaveby_is('return');
+    $trap->quiet;           # ok( !$trap->stdout && !$trap-stderr);
+## Test::Trap
+
+use Test::Exception tests => 6; # Test exception based code
 
 
 
